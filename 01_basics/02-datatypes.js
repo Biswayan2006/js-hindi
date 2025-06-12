af58c9b3bfcb330277125d503862075f80978fce
Biswayan Mazumder <biswayanmazumder2006@gmail.com>
2025-06-12 08:39:14 +0000

    new file created

---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS as newe version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // cide readability should be high
+
+console.log("biswayan")
+
+
+let name = "biswayan" // string => prefer ""
+let age = 18 // number => 2 to the power of 53
+//bigint
+let isloggesin = true // boolean => true/false
+// null => stand alone value // is an object
+// undefined => value not assigned
+// symbol => to find the uniqueness of a component 
+
+
+// object 
+
+console.log(typeof "biswayan")
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
