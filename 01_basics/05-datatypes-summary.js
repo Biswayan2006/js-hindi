61429fa7bc832ceaf2780631d88457b0b37bab99
Biswayan Mazumder <biswayanmazumder2006@gmail.com>
2025-06-23 15:21:58 +0000

    learnt stack and heap

--- 01_basics/05-datatypes-summary.js
+++ 01_basics/05-datatypes-summary.js
@@ -35,3 +35,26 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "biswa.com"
+
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+
+userTwo.email = "biswa@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
